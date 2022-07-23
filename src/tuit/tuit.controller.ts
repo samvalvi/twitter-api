1f95ade727d8896ea170bd46e837abdad6488b43
samvalvi <57542220+samvalvi@users.noreply.github.com>
2022-07-23 17:57:17 -0600

    endpoints finished

--- src/tuit/tuit.controller.ts
+++ src/tuit/tuit.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Param, Get, Post } from '@nestjs/common';
+import { Controller, Param, Get, Post, Body, HttpCode,
+  HttpStatus, Patch, Delete } from "@nestjs/common";
 import { Tuit } from 'src/entities/tuit.entity';
 
 @Controller('tuit')
@@ -7,17 +8,34 @@ export class TuitController {
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   getTuits() {
     return 'Hello World!';
   }
 
   @Get('/:id')
-  getTuit(@Param('id') id: string): string {
-    return `Tuit with id ${id}`;
+  getTuit(@Param('id') id: string): Tuit {
+    let tuit = new Tuit();
+    tuit.id = id;
+    tuit.text = 'This is a tweet!';
+    return tuit;
   }
 
   @Post()
-  postTuits() {
-    return 'New Tuit posted!';
+  @HttpCode(HttpStatus.CREATED)
+  postTuits(@Body() tuit: Tuit): Tuit {
+    return tuit;
+  }
+
+  @Patch('/:id')
+  @HttpCode(HttpStatus.OK)
+  patchTuit(@Param('id') id: string, @Body() tuit: Tuit): Tuit {
+    return tuit;
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.OK)
+  deleteTuit(@Param('id') id: string): string {
+    return `Tuit id: ${id} Deleted!`;
   }
 }
