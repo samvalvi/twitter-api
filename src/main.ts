aaa02276b6876dc95a6e58dcfcf29b4f89cd3407
samvalvi <57542220+samvalvi@users.noreply.github.com>
2022-07-24 17:35:18 -0600

    docker added

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
+import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
+    //Filter malicious data
+    whitelist: true,
+    forbidNonWhitelisted: true,
   }));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
