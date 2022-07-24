c4df22676b03304fe9f69cecc665025e1dc2c3fb
samvalvi <57542220+samvalvi@users.noreply.github.com>
2022-07-23 19:12:37 -0600

    crud finished

--- src/tuit/tuit.controller.ts
+++ src/tuit/tuit.controller.ts
@@ -1,41 +1,42 @@
 import { Controller, Param, Get, Post, Body, HttpCode,
   HttpStatus, Patch, Delete } from "@nestjs/common";
 import { Tuit } from 'src/entities/tuit.entity';
+import { TuitService } from "./tuit.service";
 
 @Controller('tuit')
 export class TuitController {
-  constructor() {
+  constructor(private readonly tuitService: TuitService) {
+
   }
+
 
   @Get()
   @HttpCode(HttpStatus.OK)
   getTuits() {
-    return 'Hello World!';
+    return this.tuitService.getTuits();
   }
 
   @Get('/:id')
+  @HttpCode(HttpStatus.OK)
   getTuit(@Param('id') id: string): Tuit {
-    let tuit = new Tuit();
-    tuit.id = id;
-    tuit.text = 'This is a tweet!';
-    return tuit;
+    return this.tuitService.getTuit(id);
   }
 
-  @Post()
+  @Post('/add')
   @HttpCode(HttpStatus.CREATED)
   postTuits(@Body() tuit: Tuit): Tuit {
-    return tuit;
+    return this.tuitService.createTuit(tuit);
   }
 
   @Patch('/:id')
   @HttpCode(HttpStatus.OK)
   patchTuit(@Param('id') id: string, @Body() tuit: Tuit): Tuit {
-    return tuit;
+    return this.tuitService.updateTuit(id, tuit);
   }
 
   @Delete('/:id')
   @HttpCode(HttpStatus.OK)
-  deleteTuit(@Param('id') id: string): string {
-    return `Tuit id: ${id} Deleted!`;
+  deleteTuit(@Param('id') id: string): Tuit {
+    return this.tuitService.deleteTuit(id);
   }
 }
--- src/tuit/tuit.service.ts
+++ src/tuit/tuit.service.ts
@@ -4,4 +4,30 @@ import { Tuit } from 'src/entities/tuit.entity';
 @Injectable()
 export class TuitService {
   private tuits: Tuit[] = [];
+
+  getTuits(): Tuit[] {
+    return this.tuits;
+  }
+
+  getTuit(id: string): Tuit {
+    return this.tuits.find(tuit => tuit.id === id);
+  }
+
+  createTuit(tuit: Tuit): Tuit {
+    this.tuits.push(tuit);
+    return tuit;
+  }
+
+  updateTuit(id: string, tuit: Tuit): Tuit {
+    const tuitToUpdate = this.tuits.find( x => x.id === id);
+    tuitToUpdate.text = tuit.text;
+    return tuitToUpdate;
+  }
+
+  deleteTuit(id: string): Tuit {
+    const tuitToDelete = this.tuits.find( x => x.id === id);
+    const index = this.tuits.indexOf(tuitToDelete);
+    this.tuits.splice(index, 1);
+    return tuitToDelete;
+  }
 }
